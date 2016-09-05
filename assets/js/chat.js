468178303d05ca8ca5da77496435cd833ad3676d
kike <enrique.barra.arias@gmail.com>
2016-09-05 11:31:51 +0000

    more changes

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -7,7 +7,6 @@ Prox.Chat = (function(P,undefined){
   var _messagesApiUrl = "/message";
   var _usersApiUrl = "/user";
   var user = "";
-  var first_connection = true;
 
   var handleNewMessage = function() {
       console.log("handleNewMessage function");
@@ -26,38 +25,30 @@ Prox.Chat = (function(P,undefined){
     console.log("logged_user:", logged_user);
     user = logged_user;
 
-    //var socket = io.connect();
-    
-    io.socket.on('connect', function() {
-      if(first_connection){
-        console.log("PRIMERA CONEXION, vamos a pedir los mensajes");
-        first_connection = false;
-        io.socket.get(_messagesApiUrl + "?sort=createdAt%20DESC", function(messages) {
-          console.log({messages: messages});
-          var arrayLength = messages.length;
-          var templ = JST["assets/templates/chat_elem.html"];
-          for (var i = 0; i < arrayLength; i++) {
-              //he creado una función timeSince y accedo a ella desde la vista
-              //otras alternativas serían extender el objeto que se le pasa, o con los datos o con un helper
-              //https://lostechies.com/derickbailey/2012/04/26/view-helpers-for-underscore-templates/
-              $("#chat_wrap").append(templ(messages[i]));
-          }
-        });
-        io.socket.get(_usersApiUrl + "?status=online&sort=createdAt%20DESC", function(users) {
-          console.log({users: users});
-          var arrayLength = users.length;
-          var templ = JST["assets/templates/user_elem.html"];
-          for (var i = 0; i < arrayLength; i++) {
-              //he creado una función timeSince y accedo a ella desde la vista
-              //otras alternativas serían extender el objeto que se le pasa, o con los datos o con un helper
-              //https://lostechies.com/derickbailey/2012/04/26/view-helpers-for-underscore-templates/
-              $("#friends_wrap").append(templ(users[i]));
-          }
-        });
-      } else {
-        console.log("first_connection era false, ya teniamos socket conectado");
+    io.socket.get(_messagesApiUrl + "?sort=createdAt%20DESC", function(messages) {
+      console.log({messages: messages});
+      var arrayLength = messages.length;
+      var templ = JST["assets/templates/chat_elem.html"];
+      for (var i = 0; i < arrayLength; i++) {
+          //he creado una función timeSince y accedo a ella desde la vista
+          //otras alternativas serían extender el objeto que se le pasa, o con los datos o con un helper
+          //https://lostechies.com/derickbailey/2012/04/26/view-helpers-for-underscore-templates/
+          $("#chat_wrap").append(templ(messages[i]));
       }
     });
+    
+    io.socket.get(_usersApiUrl + "?status=online&sort=createdAt%20DESC", function(users) {
+      console.log({users: users});
+      var arrayLength = users.length;
+      var templ = JST["assets/templates/user_elem.html"];
+      for (var i = 0; i < arrayLength; i++) {
+          //he creado una función timeSince y accedo a ella desde la vista
+          //otras alternativas serían extender el objeto que se le pasa, o con los datos o con un helper
+          //https://lostechies.com/derickbailey/2012/04/26/view-helpers-for-underscore-templates/
+          $("#friends_wrap").append(templ(users[i]));
+      }
+    });
+
 
     io.socket.on('message', function messageNotificationReceivedFromServer( message ) {
       console.log("MESSAGE notification through the socket");
